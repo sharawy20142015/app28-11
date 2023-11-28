2d4a29fe4518476de652a6a68805cd03d4ccb25e
sharawy20142015 <126897477+sharawy20142015@users.noreply.github.com>
2023-11-28 23:04:29 +0200

    Update firstapp.py

--- firstapp.py
+++ firstapp.py
@@ -15,9 +15,17 @@ def read_data():
     query='select * from users'
     df=pd.read_sql_query(query,conn)
     return df
+def clear_db():
+    conn=sqllite3.connect("sharawy.db")
+    cursor=conn.cursor()
+    cursor.execute('DELETE FROM users')
+    conn.commit()
+    conn.close()
 st.bar_chart(data=read_data(),x='name',y='age')
 title = st.text_input('Movie title', 'Life of Brian')
 if st.button('insert'):
+    clear_db()
     add_data('momom',15)
+
     
 
